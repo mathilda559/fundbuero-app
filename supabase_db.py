cfa0c3901b1ddfd03b6db921ba045518df44b60d
mathilda559 <wulfma@katharineum.de>
2026-03-12 13:40:25 +0100

    Update supabase_db.py

--- supabase_db.py
+++ supabase_db.py
@@ -1,1 +1,24 @@
+import os
+from supabase import create_client
 
+url = os.getenv("SUPABASE_URL")
+key = os.getenv("SUPABASE_KEY")
+
+supabase = create_client(url, key)
+
+def save_item(titel, kategorie, bild_url):
+
+    data = {
+        "titel": titel,
+        "kategorie": kategorie,
+        "bild_url": bild_url
+    }
+
+    supabase.table("fundstuecke").insert(data).execute()
+
+
+def load_items():
+
+    response = supabase.table("fundstuecke").select("*").execute()
+
+    return response.data
